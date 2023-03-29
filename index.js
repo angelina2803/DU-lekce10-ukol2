2817fcb36e871ccebf3f957e0157528b5c3f0ceb
Angelina Yumatova <>
2023-03-29 17:07:53 +0200

    change

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 'use strict';
+const startBudik = (event) => {
+	event.preventDefault()
+	let time = Number(document.querySelector('.time-input').value)
+	const sekundyElm = document.querySelector('.alarm__seconds')
+	const alarmElm = document.querySelector('.alarm')
 
-console.log('funguju!');
+	const odpocet = () => {
+		if (time > 0) {
+			time = time - 1
+			sekundyElm.textContent = String(time)
+		}
+		if (time <= 0) {
+			clearInterval(casovac)//zastavi budik na interval pak zaopak funkce 
+			alarmElm.classList.add('alarm--ring')
+			document.querySelector('audio').play()
+		}
+	}
+
+	const casovac = setInterval(odpocet, 500)
+}
+
+const formularElm = document.querySelector('.controls')
+formularElm.addEventListener('submit', startBudik)
